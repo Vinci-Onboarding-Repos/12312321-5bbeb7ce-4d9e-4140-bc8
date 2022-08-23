f39bc5565439ef56007bd19e60796fa56c126914
Joao <joao.airesmatos@gmail.com>
2022-08-23 15:59:18 +0100

    .

--- js/index.js
+++ js/index.js
@@ -2,15 +2,12 @@ const BASE_URL = 'http://localhost:5001/vinci-dev-6e577/us-central1/api/public/o
 
 
 const fetchUsers = () => {
-    axios.get("https://checkip.amazonaws.com/", { headers: { "Access-Control-Allow-Origin": "*" } })
-        .then(res => res.text()).then(data => console.log(data))
     console.log(window.location.href.split('/')[window.location.href.split('/').length - 1]);
+    console.log(window.location.href);
     axios.get(BASE_URL, {
         params: {
             url: window.location.href
         },
-
-        headers: { "Access-Control-Allow-Origin": "*" }
     })
         .then(response => {
             const users = response.data.data;
