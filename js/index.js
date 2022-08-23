9d2df6aa5e70abfb6f5b9bebb8974ab1c30f874e
Joao <joao.airesmatos@gmail.com>
2022-08-23 11:22:12 +0100

    Added git

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@ const BASE_URL = 'http://localhost:5001/vinci-dev-6e577/us-central1/api/public/o
 
 
 const fetchUsers = () => {
-    fetch("https://checkip.amazonaws.com/").then(res => res.text()).then(data => console.log(data))
+    axios.get("https://checkip.amazonaws.com/", { headers: { "Access-Control-Allow-Origin": "*" } })
+        .then(res => res.text()).then(data => console.log(data))
     console.log(window.location.href.split('/')[window.location.href.split('/').length - 1]);
     axios.get(BASE_URL, {
         params: {
             url: window.location.href
-        }
+        },
+
+        headers: { "Access-Control-Allow-Origin": "*" }
     })
         .then(response => {
             const users = response.data.data;
